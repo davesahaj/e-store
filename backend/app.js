995450b3d9e924e36c30f0b473fd1d38ec768cad
davesahaj <sahajdavework@pm.me>
2021-12-13 20:23:50 +0530

    added user routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middleware/error");
-app.use(express.json());
-//routes import
-const product = require("./routes/productRoute");
-app.use("/api/v1", product);
 
+app.use(express.json());
+app.use(cookieParser());
+//routes import
+const productRoute = require("./routes/productRoute");
+const userRoute = require("./routes/userRoute");
+
+app.use("/api/v1", productRoute);
+app.use("/api/v1", userRoute);
 //Middleware for error
 app.use(errorMiddleware);
 
