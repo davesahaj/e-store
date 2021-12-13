704c66b82f0fed8ddc4c5dfaceeb4e67617de7f9
davesahaj <sahajdavework@pm.me>
2021-12-13 20:24:13 +0530

    added dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const dotenv = require("dotenv").config({ path: "backend/config/config.env" });
+const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 const PORT = 4000;
 
@@ -10,11 +10,12 @@ process.on("uncaughtException", (err) => {
   process.exit();
 });
 
+dotenv.config({ path: "backend/config/config.env" });
 //connect to database
 connectDatabase();
 
-const server = app.listen(PORT, () => {
-  console.log(`Server is working on http://localhost:${PORT}`);
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
 // Unhandled Promise Rejection
