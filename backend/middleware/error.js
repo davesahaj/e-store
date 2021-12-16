9dde9f63273fce8d6a6507c198189539e0a2c81d
davesahaj <sahajdavework@pm.me>
2021-12-16 13:52:24 +0530

    error handling for authentication

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,5 +10,22 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  //mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //JSON Web Token error
+  if (err.name === "JsonWebTokenError") {
+    const message = "Json Web Token is invalid. Try again";
+    err = new ErrorHandler(message, 400);
+  }
+  //JWT Expire Error
+  if (err.name === "TokenExpiredError") {
+    const message = "Json Web Token is expired. Try again";
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({ success: false, message: err.message });
 };
