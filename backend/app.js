304f797054550b1564266431307de6db90f76e3f
davesahaj <sahajdavework@pm.me>
2021-12-20 17:08:10 +0530

    added order API

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,11 @@ app.use(cookieParser());
 //routes import
 const productRoute = require("./routes/productRoute");
 const userRoute = require("./routes/userRoute");
+const orderRoute = require("./routes/orderRoute");
 
 app.use("/api/v1", productRoute);
 app.use("/api/v1", userRoute);
+app.use("/api/v1", orderRoute);
 //Middleware for error
 app.use(errorMiddleware);
 
